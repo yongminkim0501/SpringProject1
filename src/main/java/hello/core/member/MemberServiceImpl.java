0a4327b6823251f15d9dc7c6f99b1435decc6c98
yongminkim0501 <yongmingim166@gmail.com>
2025-01-05 14:40:03 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component//컴포넌트를 사용하면 autowired를 사용할 수 밖에 없음
+        // 그러지 않으면 의존관계를 설정할 수 있는 방법이 없음.
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
+    //ac.getBean(MemberRepository.class) 이거와 같이 움직인다고 생각하면 됨
+    @Autowired // autowired라는 기능을 생성자에 붙여주면 멤버 리파지토리
+    // 타입에 맞는 애를 찾아와서 의존관계 주입을 자동으로 연결해서 주입해줌
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>(); // concurrency hash map 을 써야함, 동시성 이슈가 생길 수 있음.
     @Override
