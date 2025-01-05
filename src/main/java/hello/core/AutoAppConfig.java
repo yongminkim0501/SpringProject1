bcccc002aa3ee563c58e33656c8c3e983fe1b44a
yongminkim0501 <yongmingim166@gmail.com>
2025-01-05 14:51:41 +0900

    기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(
+@ComponentScan(//default : hello.core에 있는 모든 것을 다 뒤짐.
+        //즉, 지정하지 않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
+        //권장 방법, 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것.
+        // 최근 스프링 부트도 이 방법을 기본으로 제공함.
+        basePackages = "hello.core.member",
+        basePackageClasses = {AutoAppConfig.class},
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
